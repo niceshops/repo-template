b485060ed73ff62e67dbe9e8cd7d2c8556d794ad
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-25 10:55:26 +0200

    Exception

---
+++ src/Exception.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * @see       https://github.com/niceshops/nice-{component} for the canonical source repository
+ * @license   https://github.com/niceshops/nice-{component}/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsDev\ComponentNamespace;
+
+
+/**
+ * Class Exception
+ * @package NiceshopsDev\ComponentNamespace
+ */
+class Exception extends \NiceshopsDev\NiceCore\Exception
+{
+
+}
