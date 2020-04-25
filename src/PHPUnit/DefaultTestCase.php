14747abadd9e061c41834e47ad53ea74fbc49c9f
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-25 10:51:46 +0200

    DefaultTestCase

---
+++ src/PHPUnit/DefaultTestCase.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * @see       https://github.com/niceshops/nice-{component} for the canonical source repository
+ * @license   https://github.com/niceshops/nice-{component}/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsDev\ComponentNamespace\PHPUnit;
+
+/**
+ * Class DefaultTestCase
+ * @package NiceshopsDev\{Component}
+ */
+class DefaultTestCase extends \NiceshopsDev\NiceCore\PHPUnit\DefaultTestCase
+{
+
+}
---
+++ test/PHPUnit/DefaultTestCaseTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+/**
+ * @see       https://github.com/niceshops/nice-{component} for the canonical source repository
+ * @license   https://github.com/niceshops/nice-{component}/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsDev\ComponentNamespace\PHPUnit;
+
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * Class DefaultTestCaseTest
+ * @package NiceshopsDev\Bean
+ */
+class DefaultTestCaseTest extends \NiceshopsDev\NiceCore\PHPUnit\DefaultTestCase
+{
+    
+    
+    /**
+     * @var DefaultTestCase|MockObject
+     */
+    protected $object;
+    
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     */
+    protected function setUp()
+    {
+        $this->object = $this->getMockBuilder(DefaultTestCase::class)->disableOriginalConstructor()->getMockForAbstractClass();
+    }
+    
+    
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+    
+    
+    /**
+     * @group integration
+     * @small
+     */
+    public function testTestClassExists()
+    {
+        $this->assertTrue(class_exists(DefaultTestCase::class), "Class Exists");
+        $this->assertTrue(is_a($this->object, DefaultTestCase::class), "Mock Object is set");
+    }
+}
